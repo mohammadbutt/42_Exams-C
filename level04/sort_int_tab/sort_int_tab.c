169e4d0345dbaf77c6b336b39385d425c30c0db9
Mohammad Butt <mbutt@student.42.us.org>
2019-05-30 11:43:29 -0700

    Adding sort_int_tab

---
+++ level04/sort_int_tab/sort_int_tab.c
@@ -0,0 +1,59 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   sort_int_tab.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/30 09:49:39 by mbutt             #+#    #+#             */
+/*   Updated: 2019/05/30 11:42:55 by mbutt            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+#include <stdio.h>
+
+/*Not graded by mouli*/
+void ft_swap(int *a, int *b)
+{
+	int temp;
+	temp = *a;
+	*a = *b;
+	*b = temp;
+}
+
+/*
+** When we sort, we look back and compare the existing element with the previous
+** number [i -1]. Comparing it forward [i+1] will print garbage value for the
+** last number because when i is on the last value, [i+1] will be null. Hence,
+** printing garbage values. Values can be compared by looking forward, but
+** additional flags would have to be set in place.
+*/
+
+void sort_int_tab(int *tab, unsigned int size)
+{
+	int max;
+	int i;
+
+	i = 0;
+		while(i < size)
+		{
+			if(tab[i-1] > tab[i])
+			{
+				ft_swap(&tab[i], &tab[i-1]);
+				i = 0;
+			}
+			i++;
+		}
+}
+/*
+int main (void)
+{
+	int range[] = {4, 2, 2, 2, 1, 1, };
+	sort_int_tab(range, 6);
+	int i = 0;
+	while(i < 6)
+		printf("%d\n", range[i++]);
+	return(0);
+}
+*/
