2b56dc9a431c4796035e1abd0ebd734d88494e02
Mohammad Butt <mbutt@student.42.us.org>
2019-05-31 17:46:56 -0700

    Added paramsum

---
+++ level03/paramsum/paramsum.c
@@ -0,0 +1,43 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   paramsum.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/31 17:27:45 by mbutt             #+#    #+#             */
+/*   Updated: 2019/05/31 17:45:48 by mbutt            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+
+#include <unistd.h>
+/*Passes examshell*/
+
+/*This is a modified version of ft_putnbr, which works because we dont have to
+ *handle negative numbers/arguments
+ */
+void ft_putnbr(int nb)
+{
+	if(nb >= 10)
+		ft_putnbr(nb / 10);
+	nb = nb%10 + '0';
+	write(1, &nb, 1);
+}
+int main (int argc, char **argv)
+{
+	int i;
+
+	i = 0;
+	if(argc < 2)
+	{
+		ft_putnbr(0);
+		write(1, "\n", 1);
+		return(0);
+	}
+	while(argv[i])
+		i++;
+	i = i - 1; /*we subtract 1 from i because first argument starts at 1, not 0*/
+	ft_putnbr(i);
+	write(1, "\n", 1);
+	return(0);
+}
