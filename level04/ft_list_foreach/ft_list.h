47eea1568a28df38be1e0eca7bc49d6ca146e720
Mohammad Butt <maniqrb@gmail.com>
2019-06-02 18:01:26 -0700

    Update ft_list.h

--- level04/ft_list_foreach/ft_list.h
+++ level04/ft_list_foreach/ft_list.h
@@ -13,10 +13,10 @@
 #ifndef FT_LIST_H
 # define FT_LIST_H
 
-typedef struct			s_list
+typedef struct		s_list
 {
-	struct	s_list		*next;
-	void 				*data;
-}						t_list;
+	struct	s_list	*next;
+	void 		*data;
+}			t_list;
 
 #endif
