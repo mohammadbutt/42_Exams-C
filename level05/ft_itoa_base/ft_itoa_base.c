df8d01f4b68b536e21de855200fc3719362e4ac5
Mohammad Butt <mbutt@student.42.us.org>
2019-06-03 19:50:32 -0700

    Fixed ft_itoa_base

--- level05/ft_itoa_base/ft_itoa_base.c
+++ level05/ft_itoa_base/ft_itoa_base.c
@@ -6,7 +6,7 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/03 17:21:24 by mbutt             #+#    #+#             */
-/*   Updated: 2019/06/03 19:07:35 by mbutt            ###   ########.fr       */
+/*   Updated: 2019/06/03 19:49:48 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /*Did not push to examshell. But passes personal tests*/
@@ -20,7 +20,7 @@ int ft_numlen(int num, int base) //
 	i = 0;
 	if(base < 2 || base > 16)
 		return(0);
-	if(num <= 0 && base == 10) //
+	if(num <= 0 && base == 10)
 		i++;
 	while(num)
 	{
@@ -30,38 +30,40 @@ int ft_numlen(int num, int base) //
 	return(i);
 }
 
+int ft_abs(int n)
+{
+	if(n < 0)
+		return(-1 * n);
+	return(n);
+
+}
 char *ft_itoa_base(int value, int base) //
 {
 	int len;
-	int sign;
 	char *str;
 	char *placeholder = "0123456789ABCDEF";
 	len = ft_numlen(value, base);  //
-	sign = 1;
-	str = (char *)malloc(sizeof(char) * (len +1));
-	str[len] = '\0';
-	len--;
 	if(base < 2 || base > 16) 	//
 		return(NULL);			//
 	if(value == 0)
 		return("0");
+	str = (char *)malloc(sizeof(char) * (len +1));
+	if(str == NULL)
+		return(NULL);
 	if(value < 0 && base == 10) //
-	{
 		str[0] = '-';
-		value = -1 * value;
-	}
-	else if(value < 0 && base != 10) //
-		value = -1 * value;			//
+	str[len] = '\0';
+	len--;
 	while(value)
 	{
-		str[len] = placeholder[value%base];// + '0';
+		str[len] = placeholder[ft_abs(value%base)];// + '0';
 		value = value/base;  //
 		len--;
 	}
 	
 	return(str);
 }
-/*
+
 int main(int argc, char **argv)
 {
 	int value;
@@ -74,7 +76,5 @@ int main(int argc, char **argv)
 	base = atoi(argv[2]);
 	str = ft_itoa_base(value, base);
 	printf("%s\n", str);
-//	while(1);
 	return(0);
 }
-*/
