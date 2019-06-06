33ef2e7c7e01a8da31e1ecce99ed1e0ab058b84a
Mohammad Butt <mbutt@student.42.us.org>
2019-06-05 22:11:50 -0700

    Added is power of 2

--- level02/is_power_of_2/is_power_of_2.c
+++ level02/is_power_of_2/is_power_of_2.c
@@ -6,29 +6,33 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 15:31:39 by mbutt             #+#    #+#             */
-/*   Updated: 2019/06/05 15:55:57 by mbutt            ###   ########.fr       */
+/*   Updated: 2019/06/05 22:11:14 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*Failed examshell*/
+/*Passes examshell*/
 
 #include <stdio.h>
 
 int is_power_of_2(unsigned int n)
 {
-	if(n == 0 || n == 1)
+	if(n == 0)
+		return(0);
+	if(n == 1)
 		return(1);
 	while(n >= 2)
 	{
-		if((n % 2) == 1) 	/*If n is odd, modulo 2 will be 1.*/
-			return(0);		/*So we can end the program and return 0 because an
-							  odd number cannot be a power of 2.*/
-
-		if((n % 2) == 0) 	/*If n is even, modulo 2 will be 0.*/
-			n = n/2;		/*So we divide n by 2 and run the while loop again*/
+/* If n is odd, modulo 2 will be 1, so we end the program and return 0 because
+** an odd number cannot be a power of 2.*/
+		if((n % 2) == 1)
+			return(0);
+		if((n % 2) == 0)
+			n = n/2;
+/* If n is even, modulo 2 is 0, so we divide n by 2 and run the while loop again*/
 	}
-	return(1);				/*If n makes it this far out of the while loop then
-							  n is a power of 2.*/
+/*If n makes it this far out of the while loop then n is a power of 2 and returns 1*/
+	return(1);
 }
+
 /*
 int main (void)
 {
