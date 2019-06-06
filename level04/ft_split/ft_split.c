a668b6b7b72a5c998da02db5abfa184a3f0152a7
Mohammad Butt <mbutt@student.42.us.org>
2019-06-06 12:07:16 -0700

    Passed ft_split

--- level04/ft_split/ft_split.c
+++ level04/ft_split/ft_split.c
@@ -6,23 +6,26 @@
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2019/06/05 13:05:12 by mbutt             #+#    #+#             */
-/*   Updated: 2019/06/06 11:05:40 by mbutt            ###   ########.fr       */
+/*   Updated: 2019/06/06 12:06:54 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*Failed examshell*/
+/*Passes examshell*/
+/*
+** There are 3 main at the bottom, if you would like to test your ft_split.
+** I recommend testing second main, which will split the command line argument
+** into words. This is a good way to check if ft_split is segfaulting.
+*/
 
 #include <stdlib.h> /*malloc(3)*/
 #include <stdio.h> 	/*printf(3), fopen(3), getline(3)*/
 #include <string.h> /*strcat(3)*/
 
 /*
-** ft_stn(), function that returns 1 if it detects, whitespaces.
-** man isspace(3).
+** ft_stn(), function that returns 1 if it detects spaces/tabs/newline.
 ** Returns 0 if it doesnt detect spaces/tabs/newlines.
 */
 int ft_stn(char c)
 {
-//	if(c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r' || c  == ' ')	
 	if(c  == '\t' || c == '\n' || c == ' ')		
 		return(1);
 	return(0);
@@ -40,8 +43,8 @@ int ft_wordcount(char *str)
 	i = 0;
 	count = 0;
 	if(str == NULL)
-		return(count);	/*If string is empty, wordcount will return 0.*/
-	while(str[i]) 		/* While loop explained below in a simpler way. (2)*/
+		return(count);	/* Footnote 1.0*/
+	while(str[i]) 		/* Footnote 1.1*/
 	{
 		if(ft_stn(str[i]) == 0 && (ft_stn(str[i+1]) == 1 || str[i+1] == '\0'))
 			count++;
@@ -50,14 +53,15 @@ int ft_wordcount(char *str)
 	return(count);
 }
 /*
-** (2). Simple way to understand the above while loop is to consider that the
-** only delimiter is a single space. Inside the while loop, we write a single
-** if statement that looks at 2 things:
+** Footnote 1.0 - If string is empty worcount will return 0.
+** Footnote 1.1 - Simple way to understand the above while loop is to consider
+** that the only delimiter is a single space ' '. Inside the while loop, we write
+** a single if statement that looks at 2 things:
 ** 		1.	Current element, str[i], is not a single space character. 
 **		2.	1 element ahead of the current element, str[i+1] is either a space
-**			or a null terminated character.
-** When the above two conditions are met we do count++. Otherwise, we do i++,
-** to move to the next character of the string.
+**			or a null terminated character '\0'.
+** When the above two conditions are met we do count++. And we increment i each
+** time to move to the next character of the string.
 ** Below is what the while loop looks like with a single space as a delimiter:
 ** while(str[i])
 ** {
@@ -98,25 +102,29 @@ char **ft_split(char *str)
 		return(NULL);
 	while(word_count)
 	{
-		while(ft_stn(str[i]))
+		while(ft_stn(str[i])) /*Footnote 2.0*/
 			i++;
 		while(str[i] && ft_stn(str[i]) == 0)
-//		while(str[i] && (str[i] != ' ' && str[i] != '\t' && str[i] != '\n'))
 		{
 			i++;
 			len++;
 		}
 		word[j] = ft_strsub(str, i - len, len);
 		len = 0;
-		j++;		
-//		while(ft_stn(str[i])) /*i++ as long as str[i] is ' ', '\t', or '\n'*/
-//			i++;
+		j++;
 		word_count--;
 	}
 	word[j] = NULL;
 	return(word);
 }
 /*
+** Footnote 2.0 - If there are spaces, tabs or newlines at the start of the
+** string, we will skip all those first. There is no while loop at the end that
+** skips whitespaces since we are doing word_count--, the while loop will only
+** run as long as there is a word_count
+*/
+/*
+// Main1
 int main(void)
 {
 	int i;
@@ -146,6 +154,7 @@ int main(void)
 }
 */
 /*
+// Main2
 int main(int argc, char **argv)
 {
 	int i;
@@ -166,6 +175,7 @@ int main(int argc, char **argv)
 }
 */
 /*
+//Main3
 int main (void)
 {
 	int i;
