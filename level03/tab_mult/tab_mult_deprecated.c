b0249c0e86dd2a665b3cf60d57a6febfd556570c
Mohammad Butt <mbutt@student.42.us.org>
2019-06-10 12:18:50 -0700

    Adding tab_mult with improved ft_putnbr

--- level03/tab_mult/tab_mult_deprecated.c
+++ level03/tab_mult/tab_mult_deprecated.c
@@ -5,15 +5,34 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/10 11:36:30 by mbutt             #+#    #+#             */
-/*   Updated: 2019/06/10 12:17:43 by mbutt            ###   ########.fr       */
+/*   Created: 2019/06/02 14:03:24 by mbutt             #+#    #+#             */
+/*   Updated: 2019/06/02 15:04:59 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*Passed examshell*/
+/*Passes examshell*/
 
-#include <unistd.h>
-#include <stdlib.h> /*atoi(3)*/
+#include <unistd.h> /* write(3) */
 
+void ft_putchar(char c)
+{
+	write(1, &c, 1);
+}
+
+void ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	if(str)
+		while(str[i])
+			ft_putchar(str[i++]);
+}
+int ft_isspace(int c)
+{
+	if(c == '\t' || c == '\n' || c == '\v' || c == '\f' || c == '\r' || c == ' ')
+		return(1);
+	return(0);
+}
 int ft_atoi(char *str)
 {
 	int i;
@@ -23,61 +42,67 @@ int ft_atoi(char *str)
 	i = 0;
 	nb = 0;
 	sign = 1;
-	if(str[i] == '-' || str[i] == '+')
+	while(ft_isspace(str[i]) == 1)
+		i++;
+	if(str[i] == '-')
 	{
-		if(str[i] == '-')
-			sign = -1;
+		sign = -1;
 		i++;
 	}
+	else if(str[i] == '+')
+		i++;
 	while(str[i] >= '0' && str[i] <= '9')
-		nb = (nb * 10) + (str[i++] - '0');
+	{
+		nb = nb * 10 + (str[i] - '0');
+		i++;
+	}
 	return(nb * sign);
 }
 
 void ft_putnbr(int num)
 {
-	if(num == -2147483648)
-	{
-		write(1, "-2147483648", 11);
-		return ;
-	}
 	if(num < 0)
 	{
 		num = -1 * num;
-		write(1, "-", 1);
+		ft_putchar('-');
+//		write(1, "-", 1);
 	}
 	if(num >= 10)
 		ft_putnbr(num/10);
-	num = num % 10 + '0';
-	write(1, &num, 1);
+	ft_putchar(num % 10 + '0');
+//	num = num % 10 + '0';
+//	write(1, &num, 1);
 }
 
 void tab_mult(char *str)
 {
-	int num;
-	int i;
+	int index;
+	int number;
 	int result;
 
-	num = ft_atoi(str);
-	i = 1;
-	while(i <= 9)
+	index = 1;
+	number = ft_atoi(str);
+	result = index * number;
+	while(index <= 9)
 	{
-		result = i * num;
-		ft_putnbr(i);
-		write(1, " x ", 3);
-		ft_putnbr(num);
-		write(1, " = ", 3);
+		result = index * number;
+		ft_putnbr(index);
+		ft_putstr(" x ");
+		ft_putnbr(number);
+		ft_putstr(" = ");
 		ft_putnbr(result);
-		write(1, "\n", 1);
-		i++;
+		ft_putstr("\n");
+		index++;
 	}
 }
+
 int main(int argc, char **argv)
 {
-	if(argc < 2)
+	if(argc != 2)
 	{
-		write(1, "\n", 1);
+		ft_putchar('\n');
 		return(0);
 	}
 	tab_mult(argv[1]);
+	return(0);
 }
