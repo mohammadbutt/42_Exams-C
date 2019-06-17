daf41c2d90750ddb899c5be1775b526738b0b3ce
Mohammad Butt <mbutt@student.42.us.org>
2019-06-17 14:26:50 -0700

    Added one liner if statements in ft_range

--- level03/ft_range/ft_range.c
+++ level03/ft_range/ft_range.c
@@ -5,100 +5,53 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/05/25 20:17:55 by mbutt             #+#    #+#             */
-/*   Updated: 2019/05/25 20:56:01 by mbutt            ###   ########.fr       */
+/*   Created: 2019/06/17 13:59:41 by mbutt             #+#    #+#             */
+/*   Updated: 2019/06/17 14:24:49 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /*Passed examshell*/
 
-#include <unistd.h>
-#include <stdio.h>
-#include <stdlib.h>
+#include <stdio.h>  /*printf(3)*/
+#include <stdlib.h> /*malloc(3)*/
 
-int *ft_range(int start, int end);
-int ft_count(int start, int end);
+int ft_len(int start, int end)
+{
+	int len;
+
+	len = 0;
+	(end > start) && (len = end - start);
+	(start > end) && (len = start - end);
+	return(++len);
+}
+
+
 int *ft_range(int start, int end)
 {
-	int *array;
 	int i;
-	int count;
+	int len;
+	int *range;
 
 	i = 0;
-	count = 0;
-	count = ft_count(start, end);
-	array = (int *)malloc(sizeof(int) * (count));
-	while (count)
+	len = ft_len(start, end);
+	range = (int *)malloc(sizeof(int) * (len));
+	while(i < len)
 	{
-		if(start < end)
-			array[i++] = start++;
-		else
-			array[i++] = start--;
-		count--;
+		(start >= end) && (range[i++] = start--);
+		(end > start) && (range[i++] = start++);
 	}
-	return(array);
+	return(range);
 }
-int ft_count(int start, int end)
-{
-	int count;
-	count = 0;
 
-	if(start > end)
-		count = start - end + 1;
-	else if(end > start)
-		count = end - start + 1;
-	else
-		count = 1;
-	return(count);
-}
 /*
-int main (void)
+int main(void)
 {
-	int i;
-	int *array;
+	int start = 0;
+	int end = -3;
+	int i = 0;
+	int len = ft_len(start, end);
+	int *range = ft_range(start, end);
 
-	i = 0;
-	array = ft_range(1, 3);
-	while(i < 3)
-		printf("%d\n", array[i++]);
-	printf("Test2\n");
-	i = 0;
-	array = ft_range(-1, 2);
-	while(i < 4)
-		printf("%d\n", array[i++]);
-	printf("Test3\n");
-	i = 0;
-	array = ft_range(0, -3);
-	while(i < 4)
-		printf("%d\n", array[i++]);
-	printf("Last test\n");
-	i = 0;
-	array = ft_range(5, 5);
-	while(i < 1)
-		printf("%d\n", array[i++]);
-	return(0);
-}
-*/
-/*
-int main (void)
-{
-	int start;
-	int end;
-	int count;
-
-	count = 0;
-
-	start = 0;
-	end = -3;
-	count = ft_count(start, end);
-
-	while(count)
-	{
-		if(start < end)
-			printf("%d\n", start++);
-		else
-			printf("%d\n", start--);
-		count--;
-	}
-	return(0);
+	while(i < len)
+		printf("%d\n", range[i++]);
 }
 */
---
+++ level03/ft_range/ft_range_deprecated.c
@@ -0,0 +1,104 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_range.c                                         :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/05/25 20:17:55 by mbutt             #+#    #+#             */
+/*   Updated: 2019/05/25 20:56:01 by mbutt            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/*Passed examshell*/
+
+#include <unistd.h>
+#include <stdio.h>
+#include <stdlib.h>
+
+int *ft_range(int start, int end);
+int ft_count(int start, int end);
+int *ft_range(int start, int end)
+{
+	int *array;
+	int i;
+	int count;
+
+	i = 0;
+	count = 0;
+	count = ft_count(start, end);
+	array = (int *)malloc(sizeof(int) * (count));
+	while (count)
+	{
+		if(start < end)
+			array[i++] = start++;
+		else
+			array[i++] = start--;
+		count--;
+	}
+	return(array);
+}
+int ft_count(int start, int end)
+{
+	int count;
+	count = 0;
+
+	if(start > end)
+		count = start - end + 1;
+	else if(end > start)
+		count = end - start + 1;
+	else
+		count = 1;
+	return(count);
+}
+/*
+int main (void)
+{
+	int i;
+	int *array;
+
+	i = 0;
+	array = ft_range(1, 3);
+	while(i < 3)
+		printf("%d\n", array[i++]);
+	printf("Test2\n");
+	i = 0;
+	array = ft_range(-1, 2);
+	while(i < 4)
+		printf("%d\n", array[i++]);
+	printf("Test3\n");
+	i = 0;
+	array = ft_range(0, -3);
+	while(i < 4)
+		printf("%d\n", array[i++]);
+	printf("Last test\n");
+	i = 0;
+	array = ft_range(5, 5);
+	while(i < 1)
+		printf("%d\n", array[i++]);
+	return(0);
+}
+*/
+/*
+int main (void)
+{
+	int start;
+	int end;
+	int count;
+
+	count = 0;
+
+	start = 0;
+	end = -3;
+	count = ft_count(start, end);
+
+	while(count)
+	{
+		if(start < end)
+			printf("%d\n", start++);
+		else
+			printf("%d\n", start--);
+		count--;
+	}
+	return(0);
+}
+*/
