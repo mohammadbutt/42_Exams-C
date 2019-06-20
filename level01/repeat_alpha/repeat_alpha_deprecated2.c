b8199dd04b3b9747db24228024d78aec27a4b418
Mohammad Butt <mbutt@student.42.us.org>
2019-06-20 12:04:09 -0700

    Did repeat alpha

--- level01/repeat_alpha/repeat_alpha_deprecated2.c
+++ level01/repeat_alpha/repeat_alpha_deprecated2.c
@@ -5,11 +5,11 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/20 11:43:03 by mbutt             #+#    #+#             */
-/*   Updated: 2019/06/20 12:01:32 by mbutt            ###   ########.fr       */
+/*   Created: 2019/05/30 20:34:12 by mbutt             #+#    #+#             */
+/*   Updated: 2019/05/30 20:46:02 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*Passed examshell*/
+/*Passes examshell*/
 
 #include <unistd.h>
 
@@ -19,21 +19,32 @@ void repeat_alpha(char *str)
 	int repeat;
 
 	i = 0;
-	repeat = 1;
+	repeat = 0;
 	while(str[i])
 	{
-		(str[i] >= 'a' && str[i] <= 'z') && (repeat = str[i] - 96);
-		(str[i] >= 'A' && str[i] <= 'Z') && (repeat = str[i] - 64);
-		while(repeat--)
+		if(str[i] >= 'a' && str[i] <= 'z')
+			repeat = str[i] - 96;
+		else if(str[i] >= 'A' && str[i] <= 'Z' )
+			repeat = str[i] - 64;
+		else
+			repeat = 1;
+		while(repeat)
+		{
 			write(1, &str[i], 1);
+			repeat--;
+		}
 		i++;
-		repeat = 1;
 	}
+	write(1, "\n", 1);
 }
 
-int main(int argc, char **argv)
+int main (int argc, char **argv)
 {
-	if(argc == 2)
-		repeat_alpha(argv[1]);
-	write(1, "\n", 1);
+	if(argc != 2)
+	{
+		write(1, "\n", 1);
+		return(0);
+	}
+	repeat_alpha(argv[1]);
+	return(0);
 }
