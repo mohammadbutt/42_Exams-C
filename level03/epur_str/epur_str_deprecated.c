13bfa51bfb646baab5b081cc6260ee10b6dae893
Mohammad Butt <mbutt@student.42.us.org>
2019-06-21 11:38:36 -0700

    Added epur_str

--- level03/epur_str/epur_str_deprecated.c
+++ level03/epur_str/epur_str_deprecated.c
@@ -5,13 +5,12 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/21 10:19:23 by mbutt             #+#    #+#             */
-/*   Updated: 2019/06/21 11:37:20 by mbutt            ###   ########.fr       */
+/*   Created: 2019/06/15 19:00:47 by mbutt             #+#    #+#             */
+/*   Updated: 2019/06/15 20:59:33 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*Passed examshell*/
-
-#include <unistd.h> /*write(2)*/
+/*Should pass examshell*/
+#include <unistd.h> /*wrtie(2)*/
 
 int ft_space(char c)
 {
@@ -29,7 +28,7 @@ int ft_wordcount(char *str)
 	count = 0;
 	while(str[i])
 	{
-		if((ft_space(str[i]) == 0) && (ft_space(str[i+1]) == 1 || str[i+1] == '\0'))
+		if(ft_space(str[i]) == 0 && (ft_space(str[i+1]) == 1 || str[i+1] == '\0'))
 			count++;
 		i++;
 	}
@@ -47,9 +46,9 @@ void epur_str(char *str)
 	{
 		while(ft_space(str[i]) == 1)
 			i++;
-		while(str[i] && ft_space(str[i]) != 1)
+		while(ft_space(str[i]) == 0)
 			write(1, &str[i++], 1);
-		if((ft_space(str[i]) == 1) && (wordcount > 1))
+		if(ft_space(str[i]) == 1 && wordcount > 1)
 			write(1, " ", 1);
 		wordcount--;
 	}
@@ -61,3 +60,14 @@ int main(int argc, char **argv)
 		epur_str(argv[1]);
 	write(1, "\n", 1);
 }
+
+/*
+int main(void)
+{
+//	char *str1 = "This \t\tis \t\t\ta\t\t\t\ttest  \t\t\t";
+	char *str = "	   	AIZGnd3m   		Vz38	  7PNnd   MYSrRkjl 	\
+				 OrG8yDt 	 uqSPAC6lNa4I		 	 nlT6zEc2usa9rJdOV \
+				 SlQry		  P1B84RACH	 			";
+	printf("%d\n", ft_wordcount(str));
+}
+*/
