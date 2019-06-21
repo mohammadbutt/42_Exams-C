fc311df982cfeae497a7810c0c5485ed3634b94a
Mohammad Butt <maniqrb@gmail.com>
2019-06-21 12:24:47 -0700

    Update ft_list_foreach.c

--- level04/ft_list_foreach/ft_list_foreach.c
+++ level04/ft_list_foreach/ft_list_foreach.c
@@ -12,6 +12,8 @@
 /*Passed examshell*/
 
 #include "ft_list.h"
+#include <unistd.h> /*write(3)*/
+#include <stdlib.h> /*malloc(3)*/
 
 void ft_list_foreach(t_list *begin_list, void (*f)(void *))
 {
@@ -21,3 +23,36 @@ void ft_list_foreach(t_list *begin_list, void (*f)(void *))
 		begin_list = begin_list->next;
 	}
 }
+
+/*
+void ft_putstr(char *str)
+{
+	int i;
+
+	i = 0;
+	if(str)
+		while(str[i])
+			write(1, &str[i++], 1);
+}
+
+void	print_data(void *data)
+{
+	ft_putstr(data);
+	write(1, "\n", 1);
+}
+
+int	main(void)
+{
+	t_list *test_list;
+	test_list = malloc(sizeof(t_list));
+	test_list->data = "Hello";
+	test_list->next = malloc(sizeof(t_list));
+	test_list->next -> data = "This is a test";
+	test_list->next -> next = malloc(sizeof(t_list));
+	test_list->next -> next -> data = "Does it work?";
+	test_list->next -> next -> next = NULL;
+
+	ft_list_foreach(test_list, print_data);
+	return (0);
+}
+*/
