d63923a56df0f256abc55c55f95111037489f2f8
Mohammad Butt <mbutt@student.42.us.org>
2019-06-24 12:40:48 -0700

    Added paramsum

--- level03/paramsum/paramsum_deprecated.c
+++ level03/paramsum/paramsum_deprecated.c
@@ -5,32 +5,40 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/24 12:11:51 by mbutt             #+#    #+#             */
-/*   Updated: 2019/06/24 12:39:26 by mbutt            ###   ########.fr       */
+/*   Created: 2019/05/31 17:27:45 by mbutt             #+#    #+#             */
+/*   Updated: 2019/05/31 17:47:30 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*Passed examshell*/
 
-#include <unistd.h> /*write(2)*/
+#include <unistd.h>
+/*Passes examshell*/
 
-void ft_putnbr(int num)
+/*
+** This is a modified version of ft_putnbr, which works because we dont have to
+** handle negative numbers/arguments
+*/
+void ft_putnbr(int nb)
 {
-	if(num >= 10)
-		ft_putnbr(num / 10);
-	num = (num % 10) + '0';
-	write(1, &num, 1);
+	if(nb >= 10)
+		ft_putnbr(nb / 10);
+	nb = nb%10 + '0';
+	write(1, &nb, 1);
 }
-
-int main(int argc, char **argv)
+int main (int argc, char **argv)
 {
-	if(argc > 0 && **argv) /*1.0*/
-		ft_putnbr(argc - 1);
+	int i;
+
+	i = 0;
+	if(argc < 2)
+	{
+		ft_putnbr(0);
+		write(1, "\n", 1);
+		return(0);
+	}
+	while(argv[i])
+		i++;
+	i = i - 1; /*we subtract 1 from i because first argument starts at 1, not 0*/
+	ft_putnbr(i);
 	write(1, "\n", 1);
 	return(0);
 }
-
-/*
-** 1.0 - if statement
-** if(argc > 0 && **argv) is the same as below if statement
-** if(rgc > 0 && argv[0][0])
-*/
