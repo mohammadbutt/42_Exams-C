13b4db42648b8a6083e36a90fc301409150c415b
Mohammad Butt <mbutt@student.42.us.org>
2019-06-24 21:08:23 -0700

    Added short version of aff_a

--- level00/aff_a/aff_a_simple.c
+++ level00/aff_a/aff_a_simple.c
@@ -5,29 +5,40 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/24 20:52:16 by mbutt             #+#    #+#             */
-/*   Updated: 2019/06/24 21:06:26 by mbutt            ###   ########.fr       */
+/*   Created: 2019/05/30 11:45:57 by mbutt             #+#    #+#             */
+/*   Updated: 2019/05/30 11:53:25 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*Passed examshell*/
 
-#include <unistd.h> /*write(2)*/
+#include <unistd.h>
 
-int aff_a(char *str)
+void aff_a(char *str)
 {
 	int i;
-	
+
 	i = 0;
 	while(str[i])
-		if(str[i++] == 'a')
-			return(write(1, "a\n", 2));
-	return(write(1, "\n", 1));
+	{
+		if(str[i] == 'a')
+		{
+			write(1, "a", 1);
+			break;
+		}
+		i++;
+	}
+	write(1, "\n", 1);
 }
 
-int main(int argc, char **argv)
+int main (int argc, char **argv)
 {
+	int i;
+
+	i = 0;
 	if(argc != 2)
-		return(write(1, "a\n", 2));
-	else
-		aff_a(argv[1]);
+	{
+		write(1, "a\n", 2);
+		return(0);
+	}
+	aff_a(argv[1]);
+	return(0);
 }
