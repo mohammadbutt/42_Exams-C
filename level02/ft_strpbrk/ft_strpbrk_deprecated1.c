eaddfd2a9727203b90941d66de85a6a84d30b3ed
Mohammad Butt <mbutt@student.42.us.org>
2019-06-24 21:46:43 -0700

    Did ft_strpbrk

--- level02/ft_strpbrk/ft_strpbrk_deprecated1.c
+++ level02/ft_strpbrk/ft_strpbrk_deprecated1.c
@@ -5,36 +5,49 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/24 21:30:28 by mbutt             #+#    #+#             */
-/*   Updated: 2019/06/24 21:46:04 by mbutt            ###   ########.fr       */
+/*   Created: 2019/06/23 13:15:46 by mbutt             #+#    #+#             */
+/*   Updated: 2019/06/23 13:44:56 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 /*Passed examshell*/
 
-#include <stdio.h> /*printf(3)*/
-#include <string.h> /*strpbrk(3)*/
+#include <stdio.h>  /*printf(3)*/
+#include <string.h> /*strpbrk*/
 
+int ft_strlen(char *str)
+{
+	int i;
 
-char *ft_strpbrk(const char *s1, const char *s2)
+	i = 0;
+	if(str)
+		while(str[i])
+			i++;
+	return(i);
+}
+
+char *ft_strpbrk(const char *s1, const char *s2 )
 {
 	int i;
 	int j;
+	int len;
+
 
 	i = 0;
 	j = 0;
-
-	if(s1 == NULL || s2 == NULL)
-		return(NULL);
-	while(s1[i])
+	len = ft_strlen((char *)s1);
+	while(len)
 	{
-		while(s2[j])
+		while(s1[i])
 		{
 			if(s1[i] == s2[j])
 				return((char *)s1+i);
+			if(s2[j] == '\0')
+				break;
 			j++;
 		}
 		j = 0;
 		i++;
+		len--;
 	}
 	return(NULL);
 }
@@ -42,10 +55,9 @@ char *ft_strpbrk(const char *s1, const char *s2)
 int main(void)
 {
 	char *str1 = "abcd";
-	char *str2 = "efgha";
+	char *str2 = "efghmnbvcxz";
 
-
-	printf("   strpbrk:|%s|\n", strpbrk(str1, str2));
-	printf("ft_strpbrk:|%s|\n", ft_strpbrk(str1, str2));
+	printf("   strpbrk: |%s|\n", strpbrk(str1, str2));
+	printf("ft_strpbrk: |%s|\n", strpbrk(str1, str2));
 }
 */
