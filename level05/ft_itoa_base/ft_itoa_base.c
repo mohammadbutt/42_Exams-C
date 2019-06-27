7cac136454dda81532f3999d50981101ad437588
Mohammad Butt <mbutt@student.42.us.org>
2019-06-27 13:43:10 -0700

    Did ft_itoa_base that checks for unsigned values

--- level05/ft_itoa_base/ft_itoa_base.c
+++ level05/ft_itoa_base/ft_itoa_base.c
@@ -5,77 +5,94 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/06/06 14:55:30 by mbutt             #+#    #+#             */
-/*   Updated: 2019/06/25 15:00:55 by mbutt            ###   ########.fr       */
+/*   Created: 2019/06/27 12:35:37 by mbutt             #+#    #+#             */
+/*   Updated: 2019/06/27 13:38:45 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*Passed examshell*/
-
-#include <stdio.h>  /*printf(3)*/
+/* Passed new examshell tests that checks for unsinged values that are not base
+** of 10.
+*/
 #include <stdlib.h> /*malloc(3)*/
+#include <stdio.h>  /*printf(3) for testing*/
 
-int ft_numlen(int value, int base)
-{
-	int len;
-
-	len = 0;
-	if(base < 2 || base > 16)
-		return(0);
-	if(base == 10 && value <= 0)
-		len++;
-	while(value)
-	{
-		value = value/base;
-		len++;
-	}
-	return(len);
-}
 int ft_abs(int num)
 {
 	if(num < 0)
-		return(-1 * num);
+		num = -num;
 	return(num);
 }
-char	*ft_itoa_base(int value, int base)
+int ft_numlen(int value, int base)
 {
-	int numlen;
-	char *placeholder = "0123456789ABCDEF";
+	int i;
+	unsigned int u_value;
+
+	i = 0;
+	u_value = (unsigned int) value;
+
+	if(value == 0)
+		return(0);
+	if(value < 0 && base == 10)
+		i++;
+	while(base == 10 && value)
+	{
+		value = value/base;
+		i++;
+	}
+	while(base != 10 && u_value)
+	{
+		u_value = u_value/base;
+		i++;
+	}
+	return(i);
+}
+
+char *ft_itoa_base(int value, int base)
+{
+	int len;
+	int unsigned u_value;
+	char placeholder[] = "0123456789ABCDEF";
 	char *str;
-	numlen = ft_numlen(value, base);
-	if(base < 2 || base > 16)
-		return(NULL);
+
 	if(value == 0)
 		return("0");
-	str = (char *)malloc(sizeof(char) * (numlen + 1));
-	if(str == NULL)
+	if(base < 2 || base > 16)
 		return(NULL);
+	len = ft_numlen(value, base);
+	u_value = (unsigned int) value;
+	str = malloc(sizeof(char) * (len + 1));	
 	if(value < 0 && base == 10)
 		str[0] = '-';
-	str[numlen] = '\0';
-	numlen--;
-	while(value)
+	str[len] = '\0';
+	len--;
+	while(base == 10 && value)
 	{
-		str[numlen] = placeholder[ft_abs(value%base)];
-		value = value/base;
-		numlen--;
+		str[len] = placeholder[ft_abs(value%base)];
+		value = value / base;
+		len--;
+	}
+	while(base != 10 && u_value)
+	{
+		str[len] = placeholder[u_value%base];
+		u_value = u_value / base;
+		len--;
 	}
 	return(str);
 }
+
 /*
-int main(int argc, char **argv)
+int main(void)
 {
 	int value;
 	int base;
-	char *str;
 
-	if(argc != 3)
+	value = -2147483647;
+	base = 16;
+	printf("%s\n", ft_itoa_base(value, base));
+	while(value)
 	{
-		printf("\n");
-		return(0);
+		printf("|%s|\n", ft_itoa_base(value, base));
+		value = value / base;
 	}
-	value = atoi(argv[1]);
-	base = atoi(argv[2]);
-	str = ft_itoa_base(value, base);
-	printf("|%s|\n", str);
+
 }
 */
---
+++ level05/ft_itoa_base/ft_itoa_base_deprecated.c
@@ -0,0 +1,83 @@
+/* ************************************************************************** */
+/*                                                                            */
+/*                                                        :::      ::::::::   */
+/*   ft_itoa_base.c                                     :+:      :+:    :+:   */
+/*                                                    +:+ +:+         +:+     */
+/*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
+/*                                                +#+#+#+#+#+   +#+           */
+/*   Created: 2019/06/06 14:55:30 by mbutt             #+#    #+#             */
+/*   Updated: 2019/06/27 13:41:24 by mbutt            ###   ########.fr       */
+/*                                                                            */
+/* ************************************************************************** */
+/* Will fail the new tests in examshell because there are new tests that checks
+** if values are truly unsigned for all bases other than 10.
+*/
+
+#include <stdio.h>  /*printf(3)*/
+#include <stdlib.h> /*malloc(3)*/
+
+int ft_numlen(int value, int base)
+{
+	int len;
+
+	len = 0;
+	if(base < 2 || base > 16)
+		return(0);
+	if(base == 10 && value <= 0)
+		len++;
+	while(value)
+	{
+		value = value/base;
+		len++;
+	}
+	return(len);
+}
+int ft_abs(int num)
+{
+	if(num < 0)
+		return(-1 * num);
+	return(num);
+}
+char	*ft_itoa_base(int value, int base)
+{
+	int numlen;
+	char *placeholder = "0123456789ABCDEF";
+	char *str;
+	numlen = ft_numlen(value, base);
+	if(base < 2 || base > 16)
+		return(NULL);
+	if(value == 0)
+		return("0");
+	str = (char *)malloc(sizeof(char) * (numlen + 1));
+	if(str == NULL)
+		return(NULL);
+	if(value < 0 && base == 10)
+		str[0] = '-';
+	str[numlen] = '\0';
+	numlen--;
+	while(value)
+	{
+		str[numlen] = placeholder[ft_abs(value%base)];
+		value = value/base;
+		numlen--;
+	}
+	return(str);
+}
+/*
+int main(int argc, char **argv)
+{
+	int value;
+	int base;
+	char *str;
+
+	if(argc != 3)
+	{
+		printf("\n");
+		return(0);
+	}
+	value = atoi(argv[1]);
+	base = atoi(argv[2]);
+	str = ft_itoa_base(value, base);
+	printf("|%s|\n", str);
+}
+*/
