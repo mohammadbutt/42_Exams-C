062ae4de88f22e96c48e6c0d365e09d4dfcfdf9e
Mohammad Butt <mbutt@student.42.us.org>
2019-07-01 11:00:27 -0700

    Did fprime and passed examshell

--- level04/fprime/fprime_deprecated.c
+++ level04/fprime/fprime_deprecated.c
@@ -5,37 +5,41 @@
 /*                                                    +:+ +:+         +:+     */
 /*   By: mbutt <marvin@42.fr>                       +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2019/07/01 10:42:01 by mbutt             #+#    #+#             */
-/*   Updated: 2019/07/01 10:57:56 by mbutt            ###   ########.fr       */
+/*   Created: 2019/05/30 17:15:35 by mbutt             #+#    #+#             */
+/*   Updated: 2019/05/30 20:11:18 by mbutt            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
-/*Passed examshell*/
-/*Examshell will timeout if the function takes too long to solve*/
 
-#include <stdio.h> /*printf(3)*/
-#include <stdlib.h> /*atoi(3)*/
-
-void fprime(int num)
+#include<stdlib.h> /*for atoi*/
+#include<stdio.h>  /*for printf*/
+/*Did not submit to mouli, but passed personal tests*/
+int fprime(char *str)
 {
-	int i;
+	int num1;
+	int num2;
 
-	i = 2;
-	while(i < num)
+	num1 = atoi(str);
+	num2 = 2;
+	while(num1 > num2)
 	{
-		if(num % i == 0)
+		if(num1 % num2 == 0)
 		{
-			printf("%d*", i);
-			num = num / i;
+			printf("%d*", num2);
+			num1 = num1 / num2;
 		}
 		else
-			i++;
+			num2++;
 	}
-	printf("%d", num);
+	printf("%d\n", num1);
+	return(0);
 }
-
 int main(int argc, char **argv)
 {
-	if(argc == 2)
-		fprime(atoi(argv[1]));
-	printf("\n");
+	if(argc != 2)
+	{
+		write(1, "\n", 1);
+		return(0);
+	}
+	fprime(argv[1]);
+	return(0);
 }
