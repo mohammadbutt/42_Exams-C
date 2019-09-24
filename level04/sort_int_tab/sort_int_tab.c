0849497c1f71a06508fcbe8138ae0568d2369cb6
Mohammad Butt <maniqrb@gmail.com>
2019-09-24 12:15:57 -0700

    Update sort_int_tab.c

--- level04/sort_int_tab/sort_int_tab.c
+++ level04/sort_int_tab/sort_int_tab.c
@@ -10,6 +10,23 @@
 /*                                                                            */
 /* ************************************************************************** */
 /*Passed examshell*/
+/*
+** Sorting algorithm used here is a modified version of bubble sort.
+** Bubble sort will swap the left value with right if the left value is greater
+** than the right value. And it will keep traversing through the array to keep
+** swapping the biggest value on the left to the right, until it reaches the end,
+** bringing the biggest values to the right. Then the index will be set to 0 and
+** and sorting will be performed again and this time the second largest number
+** will continue to get swapped until its next to the biggest number.
+**
+** Sorting algorthim used here is different because everytime a swap is performed
+** the index gets set to 0. Unlike bubble sort which will perform multiple swaps
+** when traversing until it gets to the end of the array and bring the biggest
+** value to the right.
+** This sorting algorithm will only perform one swap when it traverses through
+** the array and after the swap is done index will be set to 0 and it will go
+** through the array again to perform the second swap.
+*/
 
 #include <stdio.h>
 
